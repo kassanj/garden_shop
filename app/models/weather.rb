d3f833a153d3af2402d326b5410e6b5ffcd55817
kassanj <kassandram022@gmail.com>
2016-05-22 09:13:42 -0400

    Update weather.rb

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,27 +1,27 @@
 class Weather
   def self.show(city)
-    get_weather(city)
+     get_weather(city)
   end
 
   private
 
   def self.get_weather(city)
-		Unirest.timeout(5)
-  	response = 	Unirest.get(
-	    			weather_url(city), 
-	    			headers: default_headers
-	    		)
-		response.body.first
+     Unirest.timeout(5)
+     response = Unirest.get(
+	  weather_url(city), 
+	  headers: default_headers
+     )
+     response.body.first
   end 
 
   def self.weather_url(city)
-  	"https://george-vustrey-weather.p.mashape.com/api.php?location=#{city}"
+      "https://george-vustrey-weather.p.mashape.com/api.php?location=#{city}"
   end
 
   def self.default_headers
-  	{
-  		"X-Mashape-Key" => "MKPZswVpF1mshulqTqOAw80NF56np1W3APIjsnIyE6pRErnjmI",
-  	    "Accept" => "application/json"
-  	}
+     {
+      "X-Mashape-Key" => "MKPZswVpF1mshulqTqOAw80NF56np1W3APIjsnIyE6pRErnjmI",
+      "Accept" => "application/json"
+     }
   end
 end
